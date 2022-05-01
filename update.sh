a8754575dce4b39d74c349ebb2364dfc1db85ec8
meepmaster <67827146+meepmaster360@users.noreply.github.com>
2022-05-01 10:24:47 +0100

    Add files via upload

---
+++ update.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+# Author: meepmaster
+# Date: 19-04-2022
+# Description: Enumeration
+
+
+# Color variables 
+
+RED="\033[1;31m"
+GREEN="\033[1;32m"
+NOCOLOR="\033[0m"
+
+# Time variable
+
+RIGHT_NOW=$(date +"%x %r %z")
+TIME_STAMP="Updated $RIGHT_NOW by $USER"
+
+# User root check
+
+function user() {
+	if [ $(id -u) != "0" ];then
+		echo "\n Please run this script with root user!"
+		exit 1
+	fi
+}
+
+# Internet connection check
+
+function connect() {
+	ping -c 1 -w 3 google.com > /dev/null 2>&1
+	if [ "$?" != 0 ];then
+		echo "\n This script needs an active internet connection!"
+		exit 1
+	fi
+}
+
+# Update Upgrade Cleaning
+
+function update_upgrade () {
+	# System update/upgrade
+	echo -e "${GREEN}Starting Update && Upgrade.${NOCOLOR}";sleep 1
+	echo
+	sudo dpkg --configure -a
+	sudo apt-get install -f
+	sudo apt update --fix-missing
+	sudo apt-get upgrade -y
+	sudo apt full-upgrade -y
+	sudo apt-get dist-upgrade -y
+	echo
+	echo -e "${GREEN}Update and Upgrade finished.${NOCOLOR}";sleep 1
+
+	# System cleaning
+	echo -e "${GREEN}Starting Cleaning.${NOCOLOR}";sleep 1
+	echo
+	sudo apt-get --purge autoremove
+	sudo apt-get autoclean
+	sudo apt-get clean
+	echo
+	echo -e "${GREEN}Cleaning finished.${NOCOLOR}";sleep 1
+	echo
+	echo -e "${GREEN}Be light, be Yourself...${NOCOLOR}"
+    echo
+    echo
+}
+
+# IP
+
+function IP () {
+	IP_SISTEMA=`hostname -I`
+	echo -e "\n Your IP is: $IP_SISTEMA"
+}
+
+function app_install () {
+
+# Nmap installation	
+
+	if [ ! -x "$(command -v nmap)" ];then
+        echo "[+] nmap not detected...Installing"
+        sudo apt-get install nmap -y > installing;rm installing
+else
+    echo "[+] nmap detected"
+     
+fi
+}
+
+
+# Call funtions
+
+update_upgrade
